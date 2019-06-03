f96a21c3bbc3c4dd27ee9e8d8e76c9e0baa1979e
Dragos Horodnic <dragos@horodnic.com>
2019-06-03 14:07:53 +0300

    Add 4xx responses & CORS whitelist

--- index.js
+++ index.js
@@ -1,22 +1,48 @@
-const {apiKey, domain, receiverAddress} = require('./private.js')
+const {
+    apiKey, 
+    domain, 
+    receiverAddress, 
+    CORSWhitelist
+} = require('./private.js')
 const mailgun = require('mailgun-js')({ apiKey, domain })
 
-exports.handler = (event, context, callback) => {
-    const { email, text, subject } = event
+const waitForSendCompletion = (mailOptions) => new Promise((resolve, reject) =>
+{
+    mailgun.messages().send(mailOptions, err => {
+        if (err) return reject(err)
+        console.log('Email sent')
+        resolve()
+    })
+})
+
+exports.handler = async (event, context, callback) => {
+    if (!CORSWhitelist.includes(event.headers.origin)) {
+        return callback(null, {statusCode: 401})
+    }
+    let requestJSON = JSON.parse(event.body)
+    console.log(requestJSON)
+    const { email, text, subject } = requestJSON
+    if (!email || !test || !subject) {
+        return callback(null, {statusCode: 400})
+    }
     const mailOptions = {
         from: email,
         subject,
         text,
         to: receiverAddress
     }
-
-    mailgun.messages().send(mailOptions, err => {
-        if (err) {
-            console.error(err)
-            callback(err)
-        } else {
-            console.log('Email sent')
-            callback()
+    try {
+        await waitForSendCompletion(mailOptions)    
+    } catch (e) {
+        console.error(e)
+        return callback(e)
+    }
+    var response = {
+        statusCode: 200,
+        headers: {
+           "Access-Control-Allow-Origin": event.headers.origin,
+           "Access-Control-Allow-Methods": "POST"
         }
-    })
+    }
+    callback(null, response)
 }
